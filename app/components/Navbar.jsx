b18e22c2e0353786399ac6a3600d29bdbf63e440
Jorge Oviedo <107929074+JorgeOviedo3@users.noreply.github.com>
2023-07-20 20:39:50 -0400

    Navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,23 +1,41 @@
+"use client";
 import Image from "next/image";
 import logosmall from "@/public/logosmall.webp";
 import { IoCall } from "react-icons/io5";
 import Link from "next/link";
-import { GiHamburgerMenu } from "react-icons/gi";
+import { CgClose, CgMenu } from "react-icons/cg";
+import { useState } from "react";
+
+const routes = [
+  {
+    href: "/",
+    name: "Home",
+  },
+  {
+    href: "/services",
+    name: "Services",
+  },
+  {
+    href: "/contact-us",
+    name: "Contact us",
+  },
+];
 
 export const Navbar = () => {
+  const [mobileMenu, setMobileMenu] = useState(false);
+
   return (
-    <header className="bg-gradient-to-b from-black to-black/50 backdrop-blur-md text-white shadow-lg fixed h-16 top-0 w-full z-[9999]">
-      <div className="mx-auto h-full max-w-screen-xl px-4 sm:px-6 py-2 lg:px-8">
+    <header className="bg-black text-white shadow-lg fixed top-0 w-full  z-[9999]">
+      <div className="mx-auto h-full max-w-screen-xl px-4 sm:px-6 py-3 md:py-5 lg:px-8">
         <div className="flex h-full  items-center justify-between">
-          <div className="flex-1 md:flex md:items-center md:gap-12">
-            <Link className="w-fit text-teal-600" href="/">
-              <span className="sr-only">Home</span>
+          <div className="mr-auto">
+            <Link className="max-w-[40px] text-teal-600" href="/">
               <Image
                 src={logosmall}
                 alt="chamos barber logo"
                 width={250}
                 height={250}
-                className="h-[40px] md:h-[50px] w-fit"
+                className="h-[50px] w-[40px]"
               />
             </Link>
           </div>
@@ -25,41 +43,59 @@ export const Navbar = () => {
           <div className="md:flex md:items-center md:gap-12">
             <nav aria-label="Global" className="hidden md:block">
               <ul className="flex items-center gap-6 text-sm">
-                <li>
-                  <Link className="" href="/">
-                    Home
-                  </Link>
-                </li>
-                <li>
-                  <Link className="" href="/services">
-                    Services
-                  </Link>
-                </li>
-                <li>
-                  <Link className="" href="/contact-us">
-                    Contact Us
-                  </Link>
-                </li>
+                {routes.map((route) => (
+                  <li key={`li-${route.name}`}>
+                    <Link className="" href={route.href}>
+                      {route.name}
+                    </Link>
+                  </li>
+                ))}
               </ul>
             </nav>
 
             <div className="flex items-center gap-4">
-              <div className="sm:flex sm:gap-4">
+              <div className="hidden md:flex">
                 <a
-                  className="rounded-md bg-white hover:bg-black/10 duration-300 px-3 md:px-5 py-2.5 text-sm font-medium text-black shadow flex items-center gap-2"
+                  className="flex rounded-md bg-white duration-300 px-3 md:px-5 py-2.5 text-sm font-medium text-black shadow items-center gap-2"
                   href="/"
                 >
                   <span>
                     <IoCall />
                   </span>
-                  Call Us
+                  Call Now
                 </a>
               </div>
-
-              <GiHamburgerMenu className=" md:hidden h-6 w-6 text-white" />
+              <div
+                onClick={() => setMobileMenu(!mobileMenu)}
+                className="hover:bg-white/10 cursor-pointer p-2 duration-300 rounded-full"
+              >
+                {mobileMenu ? (
+                  <CgClose className="md:hidden h-7 w-7 text-white" />
+                ) : (
+                  <CgMenu className="md:hidden h-7 w-7 text-white" />
+                )}
+              </div>
             </div>
           </div>
         </div>
+        {mobileMenu && (
+          <div className="flex md:hidden h-[calc(100vh-74px)] mt-10 items-center flex-col gap-5 bg-black">
+            {routes.map((route) => (
+              <Link href={route.href} key={route.name}>
+                {route.name}
+              </Link>
+            ))}
+            <a
+              className="flex w-fit rounded-md bg-white duration-300 px-3 py-2.5 text-sm font-medium text-black shadow items-center gap-2"
+              href="/"
+            >
+              <span>
+                <IoCall />
+              </span>
+              Call Now
+            </a>
+          </div>
+        )}
       </div>
     </header>
   );
